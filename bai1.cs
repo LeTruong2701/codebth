653c106bd61dfc022539c21006533e15bf8b27e8
LeTruong2701 <72636049+LeTruong2701@users.noreply.github.com>
2020-10-11 14:59:57 +0700

    first commit

---
+++ bai1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH1.bai1
+{
+    class Diem
+    {
+        private double x;
+        private double y;
+        public Diem()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Diem(double ox,double oy)
+        {
+            x = ox;
+            y = oy;
+        }
+        public void nhap()
+        {
+            Console.Write("Hoanh do la : ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Tung do la : ");
+            y = double.Parse(Console.ReadLine());
+        }
+        public void hienThi()
+        {
+            Console.WriteLine("Toa do la : ({0},{1})", x, y);
+        }
+        public double Khoangcach(Diem d1,Diem d2)
+        {
+            double kc = Math.Sqrt((d1.x-d2.x)* (d1.x - d2.x)+(d1.y-d2.y)* (d1.y - d2.y));
+            return kc;
+        }
+        static void Main(string[] args)
+        {
+            Diem t = new Diem();
+            t.nhap();
+            t.hienThi();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH1.bai1
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private int yearOfBirth;
+        private double salaryLevel;
+        private double basicSalary;
+        public void getId()
+        {
+            Console.WriteLine("Dinh danh nguoi lao dong : ", id);
+        }
+        public void getName()
+        {
+            Console.WriteLine("Ten cua nguoi lao dong : ", name);
+        }
+        public void getYearOfBirth()
+        {
+            Console.WriteLine("Nam sinh nguoi lao dong :", yearOfBirth);
+        }
+        public double getIncome()
+        {
+            double thunhap = salaryLevel * basicSalary;
+            return thunhap;
+        }
+        public void Hienthi()
+        {
+            Console.WriteLine("Dinh danh :"+id+"\t"+"Ten :"+name+"\t"+"Nam sinh:"+yearOfBirth+"\t"+"Luong co ban :"+basicSalary+"\t"+"Thu nhap:"+getIncome());
+        }
+        public double setSalaryLevel
+        {
+            get
+            {
+                return salaryLevel;
+            }
+            set
+            {
+                salaryLevel = value;
+            }
+        }
+        public double setBasicSalary
+        {
+            get
+            {
+                return basicSalary;
+            }
+            set
+            {
+                basicSalary = value;
+            }
+        }
+        static void Main()
+        {
+
+        }
+    }
+}
